f960c569eeb7bf9aa188e83c3ba2aa061757b00e
PsyhXxX <psyhxxx@gmail.com>
2021-09-14 20:53:50 +0300

    Add statement for example

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
@@ -17,6 +17,21 @@ public class Main {
 
         try {
             connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement statement = connection.createStatement();
+//            statement.execute("INSERT INTO users(name, lastname) VALUES ('Иван', 'Петров');");
+//            statement.execute("INSERT INTO users(name, lastname) VALUES ('Сергей', 'Лопушков');");
+
+            statement.addBatch("INSERT INTO users(name, lastname) VALUES ('Петр', 'Иванов');");
+            statement.addBatch("INSERT INTO users(name, lastname) VALUES ('Илья', 'Муромец');");
+
+            statement.executeBatch();
+            statement.clearBatch();
+
+            System.out.println(statement.isClosed());
+            statement.close();
+            System.out.println(statement.isClosed());
+
+            connection.close();
         } catch (SQLException e) {
             System.out.println("Connection Failed");
             e.printStackTrace();
