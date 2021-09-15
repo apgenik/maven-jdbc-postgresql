ad35945a3d0cb25189256712a53db709b678a989
PsyhXxX <psyhxxx@gmail.com>
2021-09-15 20:56:45 +0300

    Add properties

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,49 @@
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
+
+import static java.util.jar.Pack200.Packer.PASS;
+import static org.postgresql.jdbc.EscapedFunctions.USER;
 
 public class Main {
 
-    //  Database credentials
-    static final String DB_URL = "jdbc:postgresql://centos7postgre:5432/mydbtest";
-    static final String USER = "mydbuser";
-    static final String PASS = "123456789";
+//    //  Database credentials
+//    static final String DB_URL = "jdbc:postgresql://centos7postgre:5432/mydbtest";
+//    static final String USER = "mydbuser";
+//    static final String PASS = "123456789";
 
     public static void main(String[] args) {
-        System.out.println("HelloWorld JDBC");
+//        System.out.println("HelloWorld JDBC");
+
+        FileInputStream fileInputStream;
+        Properties properties = new Properties();
+        String db_url = null;
+        String user = null;
+        String pass = null;
 
         Connection connection;
 
         try {
-            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            fileInputStream = new FileInputStream("src\\main\\resources\\config.properties");
+            properties.load(fileInputStream);
+
+            db_url = properties.getProperty("db.host");
+            user = properties.getProperty("db.login");
+            pass = properties.getProperty("db.password");
+        }catch (IOException e){
+            System.err.println("ОШИБКА: Файл свойств отсуствует!");
+        }
+
+        try {
+            connection = DriverManager.getConnection(db_url, user, pass);
+
             Statement statement = connection.createStatement();
 //            statement.execute("INSERT INTO users(name, lastname) VALUES ('Иван', 'Петров');");
 //            statement.execute("INSERT INTO users(name, lastname) VALUES ('Сергей', 'Лопушков');");
-
             statement.addBatch("INSERT INTO users(name, lastname) VALUES ('Петр', 'Иванов');");
             statement.addBatch("INSERT INTO users(name, lastname) VALUES ('Илья', 'Муромец');");
 
@@ -37,13 +60,10 @@ public class Main {
             e.printStackTrace();
             return;
         }
-
 //        if (connection != null) {
 //            System.out.println("You successfully connected to database now");
 //        } else {
 //            System.out.println("Failed to make connection to database");
 //        }
-
-
     }
 }
